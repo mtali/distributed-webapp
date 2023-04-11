b330fc9adc3aa6559d7f4bb290ca6c7506e91ca9
mtali <emmanuelmtali@yahoo.com>
2023-04-11 17:36:18 +0300

    Select customers that are not user

--- customers/api/views.py
+++ customers/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,6 +12,6 @@ class CustomersListApiView(APIView):
     permission_classes = [DeviceIDHeaderPermission]
 
     def get(self, request):
-        customers = Customer.objects.all()
+        customers = Customer.objects.filter(Q(staff__isnull=True))
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
