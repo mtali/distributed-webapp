314e4e29c27985aa0b944655c70e227d0995a87d
mtali <emmanuelmtali@yahoo.com>
2023-06-01 02:36:39 +0300

    Fix

--- webapp/settings.py
+++ webapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-cwk+*!bzo&b%8i0#8ox%is^mc*_g4h%z&f$8ghnpl$+#bnpp_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-ead6.up.railway.app']
+ALLOWED_HOSTS = ['web-production-ead6.up.railway.app']
 
 # Application definition
 
