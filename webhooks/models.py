f840763a88cdf5babec987972340d425d7413c13
mtali <emmanuelmtali@yahoo.com>
2023-06-08 01:13:41 +0300

    Refactor code

--- webhooks/models.py
+++ webhooks/models.py
@@ -18,3 +18,6 @@ class Webhook(models.Model):
     processed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'webhooks'
